fb8ae38a23b9cadcc73612633aed237fcd8b5e8e
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-17 01:22:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,47 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+  
+  def self.new_by_name(song_name)
+    new_song = self.new
+    new_song.name = song_name
+    new_song
+  end 
+  
+  def self.create_by_name(song_name)
+    new_song = self.new
+    new_song.name = song_name
+    @@all << new_song
+    new_song
+  end 
+  
+  def self.find_by_name(song_name)
+    self.all.find {|song| song.name == song_name}
+  end 
+  
+  def self.find_or_create_by_name(song_name)
+    if find_by_name(song_name) == nil
+      create_by_name(song_name)
+    else
+      find_by_name(song_name)
+    end
+  end 
+  
+  def self.alphabetical
+    @@all.sort_by{|song| song.name}
+  end 
+  
+  def self.new_from_filename(song_name)
+    
+  end 
 
   def self.all
     @@all
